4b7df67cb81f5dab0528173b8172087c86e029ae
Dero <10-93@mail.ru>
2012-09-03 11:03:49 +0400

    firstcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstdemo::Application.config.secret_token = '1631f4870159a8bbe44738471a641c59f8cb0760b429c2bd10ef150b623768516ae0c44a9b08490c7ef7be436d3bf8b81b51599311d0797808886fe96967324e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Firstdemo::Application.config.session_store :cookie_store, key: '_firstdemo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Firstdemo::Application.config.session_store :active_record_store
